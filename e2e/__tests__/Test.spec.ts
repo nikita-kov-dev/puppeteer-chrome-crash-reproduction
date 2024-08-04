f1eef77f6011ab90089f60d8de24f637920c7e27
Nikita Kovalenko <nikitakov.dev@gmail.com>
2024-08-04 20:27:46 +0300

    Add e2e tests

---
+++ e2e/__tests__/Test.spec.ts
@@ -0,0 +1,73 @@
+import { afterEach, beforeEach, describe, test } from '@jest/globals'
+import puppeteer from "puppeteer";
+
+const EXTENSION_PATH = '../dist'
+let page = undefined
+
+describe('Import Folder Link', () => {
+  beforeEach(async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      dumpio: true,
+      slowMo: 50,
+      args: [`--disable-extensions-except=${EXTENSION_PATH}`, `--load-extension=${EXTENSION_PATH}`]
+    })
+
+    const pages = await browser.pages()
+    page = pages.at(0)
+    await page.goto(`chrome://newtab`)
+  })
+
+  afterEach(async () => {
+    page = undefined
+  })
+
+  test('Test 0', async () => {
+    // doesn't matter
+  })
+
+  test('Test 1', async () => {
+    // doesn't matter
+  })
+
+  test('Test 2', async () => {
+    // doesn't matter
+  })
+
+  test('Test 3', async () => {
+    // doesn't matter
+  })
+
+  test('Test 4', async () => {
+    // doesn't matter
+  })
+
+  test('Test 5', async () => {
+    // doesn't matter
+  })
+
+  test('Test 6', async () => {
+    // doesn't matter
+  })
+
+  test('Test 7', async () => {
+    // doesn't matter
+  })
+
+  test('Test 8', async () => {
+    // doesn't matter
+  })
+
+  test('Test 9', async () => {
+    // doesn't matter
+  })
+
+  test('Test 10', async () => {
+    // doesn't matter
+  })
+
+  test('Test 11', async () => {
+    // doesn't matter
+  })
+
+})
